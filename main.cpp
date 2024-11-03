6a345fc7ced2a4b465a1bd36d9e361646043d145
MartinRojas <rojas.martin25@yahoo.com>
2024-11-02 18:42:12 -0700

    delete file stack.h and stack.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
-#include "Stack.h"
 #include <iostream>
 
-using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
+};
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() : top(nullptr) {}
+
+    // Add element to the top of the stack
+    void push(int data) {
+        
+        // Add definitions
+    }
+
+    // Remove and return the top element of the stack
+    int pop() {
+        // Add definition for pop
+    }
+
+    // Check if the stack is empty
+    bool isEmpty() const {
+        // Add definitions
+    }
+
+    // Return the top element of the stack without removing it
+    int peek() const {
+        // Add definitions
+    }
+
+    // Destructor to free the allocated memory
+    ~Stack() {
+        while (!isEmpty()) {
+            pop(); // Pop all elements to free memory
+        }
+    }
+};
 
 int main() {
-    Stack<int> stack;
+    Stack stack;
 
     stack.push(1);
     stack.push(2);
@@ -12,9 +53,9 @@ int main() {
 
     std::cout << "Top element is: " << stack.peek() << std::endl; // Should print 3
 
-    // while (!stack.isEmpty()) {
-    //     std::cout << "Popping: " << stack.pop() << std::endl;
-    // }
+    while (!stack.isEmpty()) {
+        std::cout << "Popping: " << stack.pop() << std::endl;
+    }
 
     return 0;
 }
